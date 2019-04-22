1f912f2b068c6d88075f5b9a8c99da66d03d6794
miguelironhack <snoop_S_@hotmail.com>
2019-04-22 11:44:17 +0200

    class change on play buttons

--- version_0.1/public/js/play.js
+++ version_0.1/public/js/play.js
@@ -1,5 +1,22 @@
 const synthPads = document.querySelectorAll('.synth-pad');
 
+const c$1 = document.getElementById('synth-b').classList;
+const d1 = document.getElementById('synth-c').classList;
+const d$1 = document.getElementById('synth-d').classList;
+const e1 = document.getElementById('synth-e').classList;
+const f1 = document.getElementById('synth-f').classList;
+const f$1 = document.getElementById('synth-g').classList;
+const g1 = document.getElementById('synth-h').classList;
+const g$1 = document.getElementById('synth-i').classList;
+const a1 = document.getElementById('synth-j').classList;
+const a$1 = document.getElementById('synth-k').classList;
+const b1 = document.getElementById('synth-l').classList;
+const c2 = document.getElementById('synth-m').classList;
+const c$2 = document.getElementById('synth-n').classList;
+const d = document.getElementById('synth-o').classList;
+const d$ = document.getElementById('synth-p').classList;
+const c1 = document.getElementById('synth-a').classList;
+
 synthPads.forEach(pad => {
   pad.onclick = setupSounds;
 });
@@ -11,27 +28,16 @@ function setupSounds(e) {
 // we tyu
 // asdfghjk
 document.addEventListener('keydown', e => {
-  const c1 = (document.getElementById('synth-a').style.background =
-    'lighten($color: $click, $amount: 10)');
-  const c$1 = document.getElementById('synth-b').style;
-  const d1 = document.getElementById('synth-c').style;
-  const d$1 = document.getElementById('synth-d').style;
-  const e1 = document.getElementById('synth-e').style;
-  const f1 = document.getElementById('synth-f').style;
-  const f$1 = document.getElementById('synth-g').style;
-  const g1 = document.getElementById('synth-h').style;
-  const g$1 = document.getElementById('synth-i').style;
-  const a1 = document.getElementById('synth-j').style;
-  const a$1 = document.getElementById('synth-k').style;
-  const b1 = document.getElementById('synth-l').style;
-  const c2 = document.getElementById('synth-m').style;
-  const c$2 = document.getElementById('synth-n').style;
-  const d = document.getElementById('synth-o').style;
-  const d$ = document.getElementById('synth-p').style;
   console.log(e.key);
   console.log(c1);
   if (e.key == 'a') {
-    c1;
+    c1.add('active');
   }
 });
-a;
+document.addEventListener('keyup', e => {
+  console.log(e.key);
+  console.log(c1);
+  if (e.key == 'a') {
+    c1.remove('active');
+  }
+});
