94a408336576f0cfd80163a99c2956336db62560
lamahxx <frankonthegrill@gmail.com>
2019-04-24 14:26:22 +0200

    dff

--- version_0.1/config/auth.js
+++ version_0.1/config/auth.js
@@ -1,9 +1,10 @@
 module.exports = {
   ensureAuth: function(req, res, next) {
+    console.log(req.user.name);
     if (req.isAuthenticated()) {
       return next();
     }
-    req.flash('error_msg', 'You need to log in before proceeding');
-    res.redirect('users/login');
+    req.flash("error_msg", "You need to log in before proceeding");
+    res.redirect("users/login");
   }
 };
